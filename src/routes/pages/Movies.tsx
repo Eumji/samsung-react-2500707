25d0682864be484bc790b95cf20e4a1ba39951e7
ParkYoungWoong <thesecon@gmail.com>
2025-07-11 16:51:00 +0900

    07-11 16:50

--- src/routes/pages/Movies.tsx
+++ src/routes/pages/Movies.tsx
@@ -1,14 +1,33 @@
-import { useOutlet } from 'react-router'
-import MovieSearcher from '@/components/movies/MovieSearcher'
-import MovieList from '@/components/movies/MovieList'
+import { useQuery } from '@tanstack/react-query'
+import Loader from '@/components/Loader'
+
+interface Movie {
+  Title: string
+  Year: string
+  imdbID: string
+  Type: string
+  Poster: string
+}
 
 export default function Movies() {
-  const outlet = useOutlet()
+  const { data: movies, isLoading } = useQuery<Movie[]>({
+    queryKey: ['movies'],
+    queryFn: async () => {
+      const res = await fetch('https://omdbapi.com?apikey=7035c60c&s=batman')
+      const { Search } = await res.json()
+      return Search
+    },
+    staleTime: 1000 * 60 * 5
+  })
+
   return (
-    <main className="mx-auto max-w-[1200px] p-4">
-      <MovieSearcher />
-      <MovieList />
-      {outlet}
-    </main>
+    <>
+      {isLoading && <Loader size={100} />}
+      <ul>
+        {movies?.map(movie => {
+          return <li key={movie.imdbID}>{movie.Title}</li>
+        })}
+      </ul>
+    </>
   )
 }
